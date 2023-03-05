4c50132424c46e8f5a116b620e86ddc988fbb6a4
Renan de Andrade <renan.correa@engdb.com.br>
2023-03-05 15:39:51 -0300

    remove superfluous writeHeader golang

--- nginx/service-go/main.go
+++ nginx/service-go/main.go
@@ -11,13 +11,11 @@ func main() {
 		log.Println("Request received: ", r.Method, " ", r.URL.Path, " ")
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(`{"message": "success go"}`))
-		w.WriteHeader(200)
 	})
 
 	sm.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Request received: ", r.Method, " ", r.URL.Path, " ")
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
 	})
 
 	log.Println("Server started at host: 0.0.0.0:8090")
